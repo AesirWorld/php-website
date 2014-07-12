33b2be8e6a960a4a88370a6bde8e0eb24cabdd11
herenow <leonardoshiro@gmail.com>
2014-07-12 01:00:52 -0300

    removed example menus

--- addons/content_management/config/addon.php
+++ addons/content_management/config/addon.php
@@ -14,14 +14,14 @@
 return array(
 	// Defines how many news items to show on home page.
 	'LimitNews' => 2,
-	
+
 	// - Menu and sub-menu items. (displayed on left nav & content sub menu)
 	'MenuItems' => array(
 		'Main Menu' => array(
 			// Sample items for pages function.
-            'Downloads' => array('module' => 'pages','action'=>'content&path=downloads'),
-            'Rules' => array('module' => 'pages','action'=>'content&path=rules'),
-            'Features' => array('module' => 'pages','action'=>'content&path=features'),
+            //'Downloads' => array('module' => 'pages','action'=>'content&path=downloads'),
+            //'Rules' => array('module' => 'pages','action'=>'content&path=rules'),
+            //'Features' => array('module' => 'pages','action'=>'content&path=features'),
         ),
 		'Content Management' => array(
 			'News' => array('module' => 'news'),
@@ -38,7 +38,7 @@ return array(
             'add'   =>  'Add News',
         ),
     ),
-	
+
 	// Do not touch this.
     'FluxTables' => array(
         'NewsTable' => 'cp_news',
--- addons/content_management/themes/aesir/news/view.php
+++ addons/content_management/themes/aesir/news/view.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*-------------------------------------
 // Created by: Harrison aka CalciumKid
 ---------------------------------------
@@ -12,24 +12,27 @@
 // Contact: calciumkid@live.com.au
 -------------------------------------*/
 if (!defined('FLUX_ROOT')) exit;
-?>           
-<h2><?php echo htmlspecialchars(Flux::message('NewsHomeTitle')) ?></h2>
+?>
 <?php if($news): ?>
 <div class="newsDiv">
 	<?php foreach($news as $nrow):?>
-		<h4><?php echo $nrow->title ?></h4>
-		<div class="newsCont">
-			<span class="newsDate"><small>by <?php echo $nrow->author ?> on <?php echo date('m-d-y',strtotime($nrow->created))?></small></span>
-			<p><?php echo $nrow->body ?></p>
-			<?php if($nrow->created != $nrow->modified):?>
-				<small><?php echo htmlspecialchars(Flux::message('ModifiedLabel')) ?> : <?php echo date('m-d-y',strtotime($nrow->modified))?></small>
-			<?php endif; ?>
-			<?php if($nrow->link): ?>
-				<a class="news_link" href="<?php echo $nrow->link ?>"><small><?php echo htmlspecialchars(Flux::message('NewsLink')) ?></small></a>
-				<div class="clear"></div>
-			<?php endif; ?>
+		<div class="box3">
+			<div class="title"><?php echo $nrow->title ?></div>
+			<div class="content">
+				<div class="newsCont">
+					<p><?php echo $nrow->body ?></p>
+					<span class="newsDate"><small>by <?php echo $nrow->author ?> on <?php echo date('m-d-y',strtotime($nrow->created))?></small></span>
+					<?php if($nrow->created != $nrow->modified):?>
+						<small><?php echo htmlspecialchars(Flux::message('ModifiedLabel')) ?> : <?php echo date('m-d-y',strtotime($nrow->modified))?></small>
+					<?php endif; ?>
+					<?php if($nrow->link): ?>
+						<a class="news_link" href="<?php echo $nrow->link ?>"><small><?php echo htmlspecialchars(Flux::message('NewsLink')) ?></small></a>
+						<div class="clear"></div>
+					<?php endif; ?>
+				</div>
+			</div>
 		</div>
-	<?php endforeach; ?> 
+	<?php endforeach; ?>
 </div>
 <?php else: ?>
 	<p>
