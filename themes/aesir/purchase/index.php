33933fdb70ab5773cbae18f6492f5c7f92ce02b8
herenow <leonardoshiro@gmail.com>
2014-07-13 02:03:53 -0300

    added float left to logo

--- themes/aesir/purchase/index.php
+++ themes/aesir/purchase/index.php
@@ -1,7 +1,7 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 
 <div class="box3">
-	<div class="title">Loja</span></div>
+	<div class="title"><?php echo htmlspecialchars(Flux::message('StoreLabel')) ?></div>
 	<div class="content">
 <p>Os itens nesta loja são comprados utilizando seus <span class="keyword">créditos de doação</span>.  Estes créditos podem ser obetidos <a href="<?php echo $this->url('donate') ?>">fazendo uma doação para o servidor</a>.</p>
 <h2>Loja do <span class="shop-server-name"><?php echo htmlspecialchars($server->serverName) ?></span></h2>
@@ -20,7 +20,7 @@
 <h3>Categorias: <?php echo htmlspecialchars($categoryName) ?></h3>
 <?php endif ?>
 <?php if ($items): ?>
-<?php 
+<?php
 $evens = array();
 $odds  = array();
 foreach ($items as $i => $item) {
@@ -79,7 +79,7 @@ foreach ($items as $i => $item) {
 				</div>
 			<?php endforeach ?>
 		</td>
-		
+
 		<td width="50%">
 			<?php foreach ($odds as $i => $item): ?>
 				<div class="shop-item <?php echo (!($i % 2) ? 'even' : 'odd') ?>">
@@ -93,23 +93,23 @@ foreach ($items as $i => $item) {
 							<td>
 								<h4 class="shop-item-name"><?php echo htmlspecialchars($item->shop_item_name) ?></h4>
 								<?php if ($item->shop_item_qty > 1): ?>
-								<p class="shop-item-qty">Quantidade: <span class="qty"><?php echo number_format($item->shop_item_qty) ?></span></p>
+								<p class="shop-item-qty">Quantity: <span class="qty"><?php echo number_format($item->shop_item_qty) ?></span></p>
 								<?php endif ?>
-								<p class="shop-item-cost"><span class="cost"><?php echo number_format($item->shop_item_cost) ?></span> créditos</p>
+								<p class="shop-item-cost"><span class="cost"><?php echo number_format($item->shop_item_cost) ?></span> credits</p>
 								<p class="shop-item-info"><?php echo Markdown($item->shop_item_info) ?></p>
 								<p class="shop-item-action">
 									<?php if ($auth->actionAllowed('purchase', 'add')): ?>
-									<a href="<?php echo $this->url('purchase', 'add', array('id' => $item->shop_item_id)) ?>"><strong>Adicionar ao Carrinho</strong></a>
+									<a href="<?php echo $this->url('purchase', 'add', array('id' => $item->shop_item_id)) ?>"><strong>Add to cart</strong></a>
 									<?php endif ?>
 									<?php if ($auth->actionAllowed('item', 'view')): ?>
 									/ <?php echo $this->linkToItem($item->shop_item_nameid, 'View Item') ?>
 									<?php endif ?>
 									<?php if ($auth->allowedToEditShopItem): ?>
-									/ <a href="<?php echo $this->url('itemshop', 'edit', array('id' => $item->shop_item_id)) ?>">Modificar</a>
+									/ <a href="<?php echo $this->url('itemshop', 'edit', array('id' => $item->shop_item_id)) ?>">Modify</a>
 									<?php endif ?>
 									<?php if ($auth->allowedToDeleteShopItem): ?>
 									/ <a href="<?php echo $this->url('itemshop', 'delete', array('id' => $item->shop_item_id)) ?>"
-										onclick="return confirm('Are you sure you want to remove this item from the item shop?')">Deletar</a>
+										onclick="return confirm('Are you sure you want to remove this item from the item shop?')">Delete</a>
 									<?php endif ?>
 								</p>
 							</td>
@@ -121,6 +121,6 @@ foreach ($items as $i => $item) {
 	</tr>
 </table>
 <?php else: ?>
-<p>Atualmente não existem itens na loja.</p>
+<p>The store is empty.</p>
 <?php endif ?>
 </div></div>
