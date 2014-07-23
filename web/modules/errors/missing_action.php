909c342fb6d708ef28f91d8f7f3c22110e0ba511
herenow <leonardoshiro@gmail.com>
2014-07-23 08:33:20 -0300

    removed server path inclusing on missing view/action errors

--- web/modules/errors/missing_action.php
+++ web/modules/errors/missing_action.php
@@ -2,5 +2,4 @@
 if (!defined('FLUX_ROOT')) exit;
 
 $title = Flux::message('MissingActionTitle');
-$realActionPath = sprintf('%s/%s/%s/%s.php', FLUX_ROOT, $this->modulePath, $this->params->get('module'), $this->params->get('action'));
 ?>
--- web/themes/aesir/errors/missing_action.php
+++ web/themes/aesir/errors/missing_action.php
@@ -1,8 +1,4 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
-<div class="box3">
-	<div class="title"><?php echo htmlspecialchars(Flux::message('MissingActionHeading')) ?></div>
-	<div class="content">
+<h2><?php echo htmlspecialchars(Flux::message('MissingActionHeading')) ?></h2>
 <p><?php echo htmlspecialchars(Flux::message('MissingActionModLabel')) ?> <span class="module-name"><?php echo $this->params->get('module') ?></span>, <?php echo htmlspecialchars(Flux::message('MissingActionActLabel')) ?> <span class="module-name"><?php echo $this->params->get('action') ?></span></p>
 <p><?php echo htmlspecialchars(Flux::message('MissingActionReqLabel')) ?> <span class="request"><?php echo $_SERVER['REQUEST_URI'] ?></span></p>
-<p><?php echo htmlspecialchars(Flux::message('MissingActionLocLabel')) ?> <span class="fs-path"><?php echo $realActionPath ?></span></p>
-</div></div>
